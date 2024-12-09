33cde92e89f913ae7e755a99b00e6043023d8a6f
akashgoyal98 <akash@ngaif.com>
2024-12-09 15:59:37 +0530

    denomination

--- lib/history.dart
+++ lib/history.dart
@@ -1,6 +1,9 @@
 import 'package:denomination/Models/dinomation_entry_model.dart';
 import 'package:denomination/Services/Databasehelper.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:num_to_words/num_to_words.dart';
+import 'package:share_plus/share_plus.dart';
 
 class HistoryScreen extends StatelessWidget {
   @override
@@ -34,14 +37,71 @@ class HistoryScreen extends StatelessWidget {
               itemCount: snapshot.data!.length,
               itemBuilder: (context, index) {
                 var entry = snapshot.data![index];
-                return ListTile(
-                  title: Text(
-                    'Total Value: ₹${entry.denominations.fold(0, (sum, item) => sum + item.totalValue)}',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                  subtitle: Text(
-                    'Category: ${entry.category}\nDate: ${entry.date}\nRemarks: ${entry.remarks}',
-                    style: TextStyle(color: Colors.white),
+                return Slidable(
+                  endActionPane: ActionPane(motion: StretchMotion(), children: [
+                    SlidableAction(
+                      label: 'Edit',
+                      backgroundColor: Colors.blue,
+                      icon: Icons.edit,
+                      onPressed: (value) {},
+                    ),
+                    SlidableAction(
+                      label: 'Delete',
+                      backgroundColor: Colors.red,
+                      icon: Icons.delete,
+                      onPressed: (value) async {
+                        // Implement delete functionality
+                        await DatabaseHelper.instance
+                            .deleteDenominationEntry(entry.id!);
+                        print('Deleted entry ${entry.id}');
+                        // You can refresh the screen by using setState or Navigator.pop to go back to previous screen
+                      },
+                    ),
+                    SlidableAction(
+                      label: 'Share',
+                      backgroundColor: Colors.green,
+                      icon: Icons.share,
+                      onPressed: (value) {
+                        // Create a string for the denominations details
+                        String denominationsDetails = '';
+                        for (var denomination in entry.denominations) {
+                          denominationsDetails +=
+                              '₹${denomination.noteType} x ${denomination.numberOfNotes} = ₹${denomination.totalValue}\n';
+                        }
+
+                        // Calculate the Grand Total
+                        int grandTotal = entry.denominations
+                            .fold(0, (sum, item) => sum + item.totalValue);
+
+                        // Convert Grand Total to Words (using a package like flutter_num_words)
+                        String totalValueInWords = grandTotal.toWords();
+
+                        // Construct the full share content with Category, Denominations, Divider, Value in Words, and Grand Total
+                        String content =
+                            'Category: ${entry.category}\n\n' // Display Category first
+                            'Denominations Details:\n'
+                            '$denominationsDetails' // Show Denominations
+                            '------------------------\n' // Divider
+                            'Value in Words: $totalValueInWords\n' // Show Value in Words
+                            'Grand Total: ₹$grandTotal'; // Show Grand Total
+
+                        // Share the content
+                        Share.share(content);
+                      },
+                    ),
+                  ]),
+                  child: ListTile(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10)),
+                    tileColor: Colors.white12,
+                    title: Text(
+                      'Total Value: ₹${entry.denominations.fold(0, (sum, item) => sum + item.totalValue)}',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    subtitle: Text(
+                      'Category: ${entry.category}\nDate: ${entry.date}\nRemarks: ${entry.remarks}',
+                      style: TextStyle(color: Colors.white),
+                    ),
                   ),
                 );
               },
